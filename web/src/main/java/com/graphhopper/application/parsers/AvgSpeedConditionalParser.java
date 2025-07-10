c0854f7adca6c2b3b9369658e06eaa0bf7c3a7d0
Jade Wilson <jadewilson@microsoft.com>
2025-07-10 18:26:39 +0100

    Remove noisy logs

--- web/src/main/java/com/graphhopper/application/parsers/AvgSpeedConditionalParser.java
+++ web/src/main/java/com/graphhopper/application/parsers/AvgSpeedConditionalParser.java
@@ -8,11 +8,7 @@ import com.graphhopper.storage.IntsRef;
 
 import java.util.regex.*;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class AvgSpeedConditionalParser implements TagParser {
-    private static final Logger logger = LoggerFactory.getLogger(AvgSpeedConditionalParser.class);
     private final String encodedValueKey;
     private final DecimalEncodedValue avgSpeedEnc;
 
@@ -38,19 +34,15 @@ public class AvgSpeedConditionalParser implements TagParser {
         Matcher matcher = entryPattern.matcher(tag);
 
         while (matcher.find()) {
-            logger.info("Found matcher group: {}", matcher.group(0));
             double speed = Double.parseDouble(matcher.group(1));
             String condition = matcher.group(2).toLowerCase().replaceAll("[^a-z0-9:_]", "_").replace(":", "");
 
             String conditionKey = reverse ? "avgspeed_" + condition + "_backward"
                     : "avgspeed_" + condition + "_forward";
-            logger.info("Parsed key: '{}', encodedValueKey: '{}', speed: {}", conditionKey, encodedValueKey, speed);
 
             if (conditionKey.equals(encodedValueKey + "_forward") && !reverse) {
-                logger.info("Set FORWARD speed {} for edge {} with key {}", speed, edgeId, encodedValueKey);
                 avgSpeedEnc.setDecimal(false, edgeId, edgeIntAccess, speed);
             } else if (conditionKey.equals(encodedValueKey + "_backward") && reverse) {
-                logger.info("Set BACKWARD speed {} for edge {} with key {}", speed, edgeId, encodedValueKey);
                 avgSpeedEnc.setDecimal(true, edgeId, edgeIntAccess, speed);
             }
         }
